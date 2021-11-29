3b07ba9426de7c4f0ace2b6fca4d2d9081c3950d
khleelalifranji <khleelifranji@gmail.com>
2021-11-29 22:45:46 +0200

    first commit

---
+++ app/src/main/java/com/example/bmianalayzer/CompleteInformationActivity.java
@@ -0,0 +1,14 @@
+package com.example.bmianalayzer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompleteInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.complete_information);
+    }
+}
